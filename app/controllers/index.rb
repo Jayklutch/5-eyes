1d8a256e876aefd59e1f2ea677f90b46a491b583
Jayklutch & juliangommans <pair+jayklutch.juliangommans@devbootcamp.com>
2015-03-06 11:26:55 +1300

    added names to blinks, fixed session layout

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -14,25 +14,29 @@ get '/' do
 end
 
 get '/login' do
-  @wall_of_blinks = Blink.all
+  @session = check_session(session)
+   @wall_of_blinks = Blink.all
   @eyes = User.all.to_a
   # show logged in page
   erb :login
 end
 
 get '/sign_up' do
+  @session = check_session(session)
+  @session = check_session(session)
   #stuff for signing up
   erb :sign_up
 end
 
 post '/sign_up' do
+  @session = check_session(session)
   User.create!(username: params[:wanted_name], password: params[:access_code])
   redirect '/'
   #stuff for signing up
   end
 
 post '/login' do
-
+@session = check_session(session)
   @user = User.authenticate(params[:user_name],params[:access_code])
   if @user.nil?
     redirect '/'
@@ -44,16 +48,19 @@ post '/login' do
 end
 
 post '/blink' do
+  @session = check_session(session)
   @blink = Blink.create!(blink_text: params[:new_blink], user_id: session[:user_id])
   # sends a blink message to the DB and back.
   redirect '/login'
 end
 
 post '/wink' do
+  @session = check_session(session)
   # winks at a user
 end
 
 post '/logout' do
+  @session = check_session(session)
   session.clear
   redirect '/'
 end
