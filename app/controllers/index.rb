626d1a36a4c80fd8904761391022fc33c361a795
Jayklutch & juliangommans <pair+jayklutch.juliangommans@devbootcamp.com>
2015-03-06 13:59:59 +1300

    added purge

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,7 +16,7 @@ end
 
 get '/login' do
   @session = check_session(session)
-   @wall_of_blinks = Blink.all
+  @wall_of_blinks = Blink.all
   @eyes = User.all.to_a
   # show logged in page
   erb :login
@@ -57,7 +57,9 @@ end
 post '/blink' do
   @session = check_session(session)
   @blink = Blink.create!(blink_text: params[:new_blink], user_id: session[:user_id])
-  # sends a blink message to the DB and back.
+  if params[:new_blink] == '#purge#'
+    Blink.delete_all
+  end# sends a blink message to the DB and back.
   redirect '/login'
 end
 
